0902ff6a9c176698ec93e62a82188719c2071d05
unknown <akashrobo2@gmail.com>
2022-06-23 08:32:22 +0530

    Get Image URL

--- tandora_project/screens/MyPosts.js
+++ tandora_project/screens/MyPosts.js
@@ -1,6 +1,7 @@
 import React,{Component} from "react";
 import {View,Text,StyleSheet,TouchableOpacity} from 'react-native';
 import axios from "axios";
+import AsyncStorage from "@react-native-community/async-storage";
 
 export default class MyPosts extends Component {
 
@@ -19,8 +20,13 @@ export default class MyPosts extends Component {
 
 
 
-            await axios.get('https://tandora.herokuapp.com/api/upload/files/count')
-            .then((res) => console.log(res))
+            await axios.get('https://tandora.herokuapp.com/api/upload/files/')
+            .then(res => {
+                for(let i=0;i<res.data.length;i++) {
+                    console.log(res.data[i].url)
+                }
+                //console.log(res.data[0].url)
+            })
             .catch((e) => console.log(e))
         }
 
